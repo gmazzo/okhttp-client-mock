06e55be73286d6b98811a3e9ccf2dd4ebc8a9aa6
guillermo.mazzola <guillermo.mazzola@globant.com>
2017-09-15 01:05:28 -0300

    Added some HttpCodes and MediaTypes

--- src/main/java/okhttp3/mock/HttpCodes.java
+++ src/main/java/okhttp3/mock/HttpCodes.java
@@ -6,11 +6,23 @@ public interface HttpCodes {
 
     int HTTP_NO_CONTENT = 204;
 
+    int HTTP_FOUND = 302;
+
+    int HTTP_SEE_OTHER = 303;
+
+    int HTTP_NOT_MODIFIED = 304;
+
+    int HTTP_BAD_REQUEST = 400;
+
     int HTTP_UNAUTHORIZED = 401;
 
     int HTTP_FORBIDDEN = 403;
 
     int HTTP_NOT_FOUND = 404;
+
+    int HTTP_METHOD_NOT_ALLOWED = 405;
+
+    int HTTP_CONFLICT = 409;
 
     int HTTP_INTERNAL_SERVER_ERROR = 500;
 
--- src/main/java/okhttp3/mock/MediaTypes.java
+++ src/main/java/okhttp3/mock/MediaTypes.java
@@ -6,7 +6,15 @@ public interface MediaTypes {
 
     MediaType TYPE_PLAIN_TEXT = MediaType.parse("text/plain");
 
+    MediaType TYPE_HTML = MediaType.parse("text/html");
+
+    MediaType TYPE_XML = MediaType.parse("text/xml");
+
     MediaType TYPE_JSON = MediaType.parse("application/json");
+
+    MediaType TYPE_FORM_DATA = MediaType.parse("multipart/form-data");
+
+    MediaType TYPE_FORM_URLENCODED = MediaType.parse("application/x-www-form-urlencoded");
 
     MediaType TYPE_OCTET_STREAM = MediaType.parse("application/octet-stream");
 
