1419c23fc2c01bac774140ba18d1bdd7080bbf6d
canthonyl <cheung.yi.lung@gmail.com>
2021-09-22 17:52:31 +0800

    Apply suggestions from code review
    Co-authored-by: Guillermo Mazzola <guillermo.mazzola@glovoapp.com>

--- library/src/main/java/okhttp3/mock/matchers/BodyMatcher.java
+++ library/src/main/java/okhttp3/mock/matchers/BodyMatcher.java
@@ -7,13 +7,19 @@ import java.util.regex.Pattern;
 
 public class BodyMatcher extends PatternMatcher  {
 
-    public BodyMatcher(Pattern pattern) {
+    public BodyMatcher(Predicate<RequestBody> condition) {
         super(pattern);
     }
 
     @Override
     protected String getText(Request request) {
-        return RequestCache.INSTANCE.requestBody();
+        RequestBody body = request.requestBody();
+        check(!body.isDuplex()) { "duplex bodies can't be matched" }
+        check(!body.isOneShoot()) { "onShoot bodies can't be matched" }
+
+        Buffer buffer = new Buffer()
+        body.writeTo(buffer)
+        return buffer.readString(charset);
     }
 
     @Override
