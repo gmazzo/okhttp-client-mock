ed78d8da2c21e8c64dce8e16a50f0cb720471e3f
Guillermo Mazzola <gmazzo65@gmail.com>
2025-03-28 21:56:36 +0100

    Replaced versioning plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     alias(libs.plugins.kotlin) apply false
-    alias(libs.plugins.axion.release)
+    alias(libs.plugins.gitVersion)
     alias(libs.plugins.publicationsReport)
 }
 
 allprojects {
     group = "com.github.gmazzo.okhttp.mock"
-    version = rootProject.scmVersion.version
 
     tasks.withType<JacocoReport>().configureEach {
         reports.xml.required = true
